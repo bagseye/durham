542fdabc1502227e8400aa7548aa3ef0699348c3
Morgan <morganbakerdev@gmail.com>
2022-06-19 16:26:08 +0100

    Added Terser to prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
@@ -7,6 +8,7 @@ module.exports = merge(common, {
   optimization: {
     minimize: true,
     minimizer: [
+      new TerserPlugin(),
       new CssMinimizerPlugin(),
     ],
   },
